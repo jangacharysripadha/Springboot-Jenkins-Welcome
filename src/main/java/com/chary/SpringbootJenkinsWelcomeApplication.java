5b9711bc09ddaea0256050feb4aed02514a2e936
jangachary <schary1391@gmail.com>
2026-03-20 10:41:39 +0530

    First commit

---
+++ src/main/java/com/chary/SpringbootJenkinsWelcomeApplication.java
@@ -0,0 +1,14 @@
+package com.chary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootJenkinsWelcomeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootJenkinsWelcomeApplication.class, args);
+		System.out.println("welcome to springboot jenkins.........");
+	}
+
+}
---
+++ src/main/java/com/chary/controller/WelcomeController.java
@@ -0,0 +1,16 @@
+package com.chary.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WelcomeController {
+	
+	@GetMapping("/welcome")
+	public String getWelcome()
+	{
+		return "welcome to Jenkins.......";
+	}
+
+}
